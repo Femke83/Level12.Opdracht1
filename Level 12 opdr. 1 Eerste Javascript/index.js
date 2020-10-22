77619b61f7a04bc847dd5d2d5c6f68f5f867f748
Femke <64926517+Femke83@users.noreply.github.com>
2020-10-22 13:35:13 +0200

    Add files via upload

---
+++ Level 12 opdr. 1 Eerste Javascript/index.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy")
+let name = "Femke";
+console.log("Femke")
+let number = 4 + 4;
+console.log(number)
+let numberTwo = "4" + "4";
+console.log(numberTwo)
+let numberSubtraction = 3 - 9;
+console.log(numberSubtraction)
+let numberMultiplication = 56 * 825;
+console.log(numberMultiplication)
+let age = 37;
+console.log(age)
+let ageTekst = zevenendertig;
+console.log(ageTekst)
